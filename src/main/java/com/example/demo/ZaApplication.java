85609d56086893bad92d6f769639a59d6e23632e
USER <USER@DESKTOP-C4KBSR2>
2022-05-04 14:48:08 +0900

    first commit....

---
+++ src/main/java/com/example/demo/ZaApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZaApplication.class, args);
+	}
+
+}
